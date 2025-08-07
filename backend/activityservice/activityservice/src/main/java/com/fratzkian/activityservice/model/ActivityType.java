e9e23af0f65c567eb20eda9fcef6c7c808738457
KianFratz <fratzkian@gmail.com>
2025-08-07 23:18:48 +0800

    Done with Activity model and connecting mongodb to User activity service

---
+++ backend/activityservice/activityservice/src/main/java/com/fratzkian/activityservice/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fratzkian.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    LIFTING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
