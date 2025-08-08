1f1db2a280f99891f7338a61afea221bc45a76fc
KianFratz <fratzkian@gmail.com>
2025-08-08 21:15:37 +0800

    Done track activity

---
+++ backend/activityservice/activityservice/src/main/java/com/fratzkian/activityservice/controller/ActivityController.java
@@ -0,0 +1,25 @@
+package com.fratzkian.activityservice.controller;
+
+import com.fratzkian.activityservice.dto.ActivityRequest;
+import com.fratzkian.activityservice.dto.ActivityResponse;
+import com.fratzkian.activityservice.model.Activity;
+import com.fratzkian.activityservice.service.ActivityService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/activities")
+@AllArgsConstructor
+public class ActivityController {
+
+    private ActivityService activityService;
+
+    @PostMapping
+    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request) {
+        return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+}
