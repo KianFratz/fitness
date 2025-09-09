de900884d275ff5cab868042972655a1672cceb2
KianFratz <fratzkian@gmail.com>
2025-09-10 01:01:14 +0800

    Added react router and Material UI. And made a frontend for user login (not yet done).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-
+import { BrowserRouter } from "react-router";
+import { Button } from "@mui/material"
 
 function App() {
 
   return (
-    <>
-
-      
-    </>
+    <BrowserRouter>
+      <Button variant="contained">Log in</Button>
+    </BrowserRouter>
   )
 }
 
