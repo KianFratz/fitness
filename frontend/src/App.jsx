580355ab2c7d6c174642aed3ddf6f9ce103b7422
KianFratz <fratzkian@gmail.com>
2025-09-11 22:01:32 +0800

    Done connecting keycloak to user authentication login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,40 @@
 import { BrowserRouter } from "react-router";
-import { Button } from "@mui/material"
+import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./store/authSlice";
 
 function App() {
+  const { token, tokenData, logIn, logOut, isAuthenticated } =
+    useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState();
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
 
   return (
     <BrowserRouter>
-      <Button variant="contained">Log in</Button>
+      {!token ? (<Button
+        variant="contained"
+        color="secondary"
+        onClick={() => {
+          logIn();
+        }}
+      >
+        Log in
+      </Button>) : (<div>
+        <pre>{JSON.stringify(tokenData, null,2)}</pre>
+        <pre>{JSON.stringify(token, null, 2)}</pre>
+      </div>)}
+      
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
